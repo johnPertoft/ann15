b20a4e5ee6ebbbd094ccaa6d45eb4f7290a9f29b
John Pertoft <john@pertoft.se>
2015-01-21 14:57:22 +0100

    delta update function done

--- lab1/delta.m
+++ lab1/delta.m
@@ -4,15 +4,29 @@ function [ output_args ] = delta(patterns, targets, epochs)
 [insize, ndata] = size(patterns);
 [outsize, ndata] = size(targets);
 
-%W = randn(
-eta = 1.0; %????
+W = randn(outsize, insize + 1);
+eta = 0.001;
 X = [patterns; ones(1, ndata)];
 
 for i=1:epochs
-    deltaW = -eta * (WX - targets) * X';
+    deltaW = -eta * (W*X - targets) * X';
     W = W + deltaW;
     
+    %%%%
+    p = W(1,1:2);
+    k = -W(1, insize+1) / (p*p');
+    l = sqrt(p*p');
+    axis ([-2, 2, -2, 2], 'square');
+    plot (patterns(1, find(targets>0)), ...
+        patterns(2, find(targets>0)), '*', ...
+        patterns(1, find(targets<0)), ...
+        patterns(2, find(targets<0)), '+', ...
+        [p(1), p(1)]*k + [-p(2), p(2)]/l, ...
+        [p(2), p(2)]*k + [p(1), -p(1)]/l, '-');
     
+    pause(0.2);
+    drawnow;
+    %%%
 end
 
 end
