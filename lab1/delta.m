bca08a641a07dd167aa87adfd6d7e518f5149b0b
John Pertoft <john@pertoft.se>
2015-01-21 13:47:54 +0100

    gitignore

--- lab1/delta.m
+++ lab1/delta.m
@@ -1,6 +1,19 @@
 % computes blah blah
-% assumes global variables patterns and targets exist
-function [ output_args ] = delta( input_args )
+function [ output_args ] = delta(patterns, targets, epochs)
+
+[insize, ndata] = size(patterns);
+[outsize, ndata] = size(targets);
+
+%W = randn(
+eta = 1.0; %????
+X = [patterns; ones(1, ndata)];
+
+for i=1:epochs
+    deltaW = -eta * (WX - targets) * X';
+    W = W + deltaW;
+    
+    
+end
 
 end
 
