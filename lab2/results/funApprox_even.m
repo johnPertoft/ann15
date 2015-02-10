ba8763e2ed319c280f424bfa52cbe4ca7ca35ba6
Joakim Candefors <joakim.candefors@gmail.com>
2015-02-10 16:05:52 +0100

    even distribude rbf-setup

---
+++ lab2/results/funApprox_even.m
@@ -0,0 +1,56 @@
+clear;
+
+[xtrain, ytrain] = readxy('ballist', 2, 2);
+[xtest, ytest] = readxy('balltest', 2, 2);
+%units needs to be a square
+units = 25;
+data = xtrain;
+%vqinit;
+% singlewinner = 0;
+% emiterb
+%     
+% hold on
+% plot(xtest(:, 1), xtest(:, 2), 'r+')
+% hold off
+
+col = (0.5/sqrt(units):1/sqrt(units):1-(0.5/sqrt(units)));
+
+[m1 m2] = meshgrid(col, col);
+m = [m1(:) m2(:)];
+var = ones(units,1).*(0.25/units);
+delta=0.1;
+p1=1;	%temporarily
+p2=2;	%temporarily
+h=figure();
+plotdata2(h,data);
+plotrbf2(h,m,var,p1,p2);
+hold on
+plot(xtest(:, 1), xtest(:, 2), 'r+');
+hold off
+
+Phi = calcPhi(xtrain, m, var);
+
+d1 = ytrain(:,1);
+d2 = ytrain(:,2);
+dtest1 = ytest(:,1);
+dtest2 = ytest(:,2);
+
+% Weight vectors
+w1 = Phi\d1;
+w2 = Phi\d2;
+
+% Function approximation using the weigh vectors, training data
+y1 = Phi*w1;
+y2 = Phi*w2;
+
+% Function approximation using the weigh vectors, test data
+Phitest = calcPhi(xtest, m, var);
+ytest1 = Phitest * w1;
+ytest2 = Phitest * w2;
+
+figure;
+
+subplot(2,2,1), xyplot(d1, y1, 'train1');
+subplot(2,2,2), xyplot(d2, y2, 'train2');
+subplot(2,2,3), xyplot(dtest1, ytest1, 'test1');
+subplot(2,2,4), xyplot(dtest2, ytest2, 'test2');
